18c42320c70fd39989287e16fc6bb2b4fe9bb130
Shubhamyyy <102275222+Shubhamyyy@users.noreply.github.com>
2024-09-13 21:44:46 +0530

    blogs to blog

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='w-full border shadow-md py-3 fixed top-0 bg-white'>
       <header className='text-center'>
-        <h1 className='text-2xl font-bold '>BLOGS <span className='underline text-xs italic'>by Orderyyy</span></h1>
+        <h1 className='text-2xl font-bold '>BLOG <span className='underline text-xs italic'>by Orderyyy</span></h1>
       </header>
     </div>
   )
